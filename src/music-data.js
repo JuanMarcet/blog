1ccd2141ccb8ceb7597ab2b3806cd007147a63fc
Juan Marcet <juanmarcet06@gmail.com>
2025-10-17 00:02:28 -0500

    flotabilidad de música e integración de músicas

--- src/music-data.js
+++ src/music-data.js
@@ -5,19 +5,19 @@ export const musicTracks = [
         id: '1', 
         name: 'Mujeres divinas (DARIO)', 
         // ID extraído: 1G7sR6X8uqdIzhcphap5cLP1iDAOLJ9r9
-        url: 'https://drive.google.com/uc?export=download&id=1G7sR6X8uqdIzhcphap5cLP1iDAOLJ9r9' 
+        url: 'https://blog-agil.netlify.app/assets/musicas/mujeres_divinas.mp3' 
     },
     { 
         id: '2', 
         name: 'Preso (DARIO)', 
         // ID extraído: 1n6X79_JcV1mxN11jPdPle3j7SM25nje5
-        url: 'https://drive.google.com/uc?export=download&id=1n6X79_JcV1mxN11jPdPle3j7SM25nje5' 
+        url: 'https://blog-agil.netlify.app/assets/musicas/preso.mp3' 
     },
     { 
         id: '3', 
         name: 'Un millón de primaveras (ANDREA)', 
         // ID extraído: 1-YiLAtG3mvQzLo6LwR0awcRnsXvk6ML0
-        url: 'https://drive.google.com/uc?export=download&id=1-YiLAtG3mvQzLo6LwR0awcRnsXvk6ML0' 
+        url: 'https://blog-agil.netlify.app/assets/musicas/un_millon_de_primaveras.opus' 
     },
     { 
         id: '4', 
